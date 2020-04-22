83ae1fa098d638278d8c9209cd9c6887d70f71ff
diptendudas <diptendu.tu@gmail.com>
2020-04-22 17:46:30 +0530

    Post-List - MatExpansionModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {MatInputModule} from '@angular/material/input';
 import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,19 +14,21 @@ import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule, MatCardModule,MatButtonModule,MatToolbarModule
+    MatInputModule, MatCardModule,MatButtonModule,MatToolbarModule,MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls:['./post-list.component.css']
+
+})
+export class PostListComponent
+{
+
+  posts= [
+    {title:'First Post', content: ' This is First post content'},
+    {title:'Second Post', content: ' This is Second post content'},
+    {title:'Third Post', content: ' This is Third post content'}
+  ]
+
+}
