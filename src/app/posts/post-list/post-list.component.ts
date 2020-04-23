bbd55729ee27ca59b86b15f2538e59720e1fde35
diptendudas <diptendu.tu@gmail.com>
2020-04-23 10:55:57 +0530

    Model calss ( Angular Interface)

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import {Post} from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -15,5 +16,5 @@ export class PostListComponent
   //   {title:'Third Post', content: ' This is Third post content'}
   // ]
 
-  @Input() posts= [];
+  @Input() posts : Post[]= [];
 }
