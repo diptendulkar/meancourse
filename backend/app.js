8275cad5152948d1475103b2307bdc22153169eb
diptendudas <diptendu.tu@gmail.com>
2020-04-28 12:32:38 +0530

    create api/post

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 
-// this is middleware
+// this is middleware Optional
 app.use((req, res, next) => {
 
   console.log('First midleware');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/api/posts',(req, res, next) => {
 
-  res.send('Hello from Express ! SERVER IS RUNNING ...');
+  const posts =[
+    {id: 'fad123', title:'First Server-side post', content:'this  First code is comming from server'},
+    {id: 'fad124', title:'Second Server-side post', content:'this Second code is comming from server'}
+  ];
+
+  res.status(200).json({
+    message: 'post fetched sucessfull !',
+    posts: posts
+  });
 });
 
 module.exports = app;
