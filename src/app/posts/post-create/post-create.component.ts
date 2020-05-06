f5abe66a38633c3493fba17206fba44d02882494
diptendudas <diptendu.tu@gmail.com>
2020-05-06 20:02:46 +0530

    adding image control to store the image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -25,11 +25,14 @@ private postId : string;
 
 ngOnInit(){
 
+  //initilize the Reactive form ( new approach)
   this.form = new FormGroup({
     'title' : new FormControl(null,{
       validators : [Validators.required, Validators.minLength(3) ]
     }),
-    'content' :new FormControl(null,{ validators : [Validators.required] })
+    'content' :new FormControl(null,{ validators : [Validators.required] }),
+    'image' : new  FormControl(null, {validators: [Validators.required]})
+
   });
 
   this.route.paramMap.subscribe( (paramMap : ParamMap) =>{
@@ -64,6 +67,16 @@ onSavePost(){
     this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
   }
   this.form.reset();
+
+
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement ).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
 
 
   }
