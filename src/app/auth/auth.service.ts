f23ed78d97992ed922c1278313a58a4213d30b32
diptendudas <diptendu.tu@gmail.com>
2020-05-07 21:15:23 +0530

    signup- connecting  UI and Backend DB

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Subject, from} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+import {AuthDataModel} from './auth-data.model';
+
+@Injectable({providedIn: 'root'}) // to add the service/ Provider into app.module
+export class AuthService {
+
+  constructor(private http: HttpClient){}
+
+  createUser(email: string, password: string){
+    const authData: AuthDataModel = {email: email, password: password};
+
+    this.http.post("http://localhost:3000/api/user/signup", authData)
+    .subscribe( response => {
+      console.log(response);
+    });
+  }
+}
