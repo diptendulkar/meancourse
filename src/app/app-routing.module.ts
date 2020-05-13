dcfd82ddfcdc7f9aac552c59685147e6747f0fd5
diptendudas <diptendu.tu@gmail.com>
2020-05-13 17:28:11 +0530

    Auth Guard . to protect  non login page display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth-guard';
 
 
 const routes: Routes = [
   // list of all pages
   {path: '', component: PostListComponent},
-  {path: 'create', component: PostCreateComponent},
-  {path: 'edit/:postId', component: PostCreateComponent},
+  {path: 'create', component: PostCreateComponent, canActivate: [AuthGuard]}, // protect auth guard/ non login session
+  {path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]}, //protect auth Guard/ non login session
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent}
 ];
@@ -18,6 +19,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
