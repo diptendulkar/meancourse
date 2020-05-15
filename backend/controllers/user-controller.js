363ccafe231b52182f16335d89e9cc2bfe81b6ad
diptendudas <diptendu.tu@gmail.com>
2020-05-15 17:21:19 +0530

    Optimized  Backed - using Controllers

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,75 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const MongoUser = require("../models/user");
+
+exports.createUser =  (req, res, next) => {
+  // hash the password
+  bcrypt.hash(req.body.password, 10)
+  .then(hashPwd => {
+    const user = new MongoUser({
+      email: req.body.email,
+      password: hashPwd
+    });
+    // save data to mongo DB
+    user.save()
+    .then(result => {
+
+      res.status(201).json({
+        message: 'User created',
+        result: result
+      });
+
+    })
+    .catch( err => {
+      res.status(500).json({
+         message: "Email id not Available !!"
+      });
+    });
+  });
+
+
+}
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+MongoUser.findOne({ email: req.body.email})
+  .then(user => {
+    if(!user){
+      return res.status(401).json({
+        message: "Authentication Failed wrong email !"
+      });
+    }
+ fetchedUser = user; // if email exist the mongo return user obj
+    return bcrypt.compare(req.body.password, fetchedUser.password); // compair has password
+  })
+  .then(result => {
+    console.log("pass comp: " + result);
+    if(!result){
+      return res.status(401).json({
+        message: "Authentication Failed  wrong password!"
+      });
+    }
+        // Valid User - create JSon Web token - JWT
+        //console.log("fetchedUser: " + fetchedUser);
+        const token = jwt.sign(
+          {email: fetchedUser.email, userId: fetchedUser._id},
+          "diptendu_password", // secret private key
+          {expiresIn: "1h"} // expires in one hour
+          );
+          //console.log("token = " + token);
+          res.status(200).json({
+            token: token,  // send the token to forntend
+            message : " token generate sucessfull",
+            expiresIn: 3600, // in seconds
+            userId: fetchedUser._id
+
+          });
+  })
+  .catch(err => {
+    return res.status(401).json({
+      message: "Authentication Failed !"
+    });
+  });
+
+}
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const multer =  require('multer');
 
-const MongoPost = require('../models/post');
+
 const checkAuth = require('../middleware/check-auth');
 const router = express.Router();
+
+const PostController = require("../controllers/posts-controller");
 
 const MIME_TYPE_MAP ={
 'image/png': 'png',
@@ -28,139 +30,24 @@ const storage = multer.diskStorage({
 });
 
 //add a new post
-router.post("", checkAuth ,multer({storage: storage}).single("image"),(req, res,next) =>{
-
-  const url = req.protocol + '://' + req.get("host");
-  const post = new MongoPost({
-    title : req.body.title,
-    content : req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-// save  data to DB
-  post.save().then( createdPost =>{
-    console.log(createdPost);
-    res.status(201).json({
-      message: 'Post Added sucussfully !!',
-      post: {
-        id: createdPost._id, // map the id
-        ...createdPost  // set rest of the property automatically
-
-      }
-    });
-  })
-  .catch(error => {
-    res.status(500).json({
-      message: "Creating Post Failed !!"
-    });
-  })
-  ;
-});
+router.post("", checkAuth ,multer({storage: storage}).single("image"),PostController.addNewPost);
 
 
 
 // fetch all data
-router.get('',(req, res, next) => {
-
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = MongoPost.find();
-  let fetchedPosts;
-  if( pageSize && currentPage)
-  {
-    postQuery.skip(pageSize * (currentPage -1))
-    .limit(pageSize);
-  }
-  postQuery.then( documents => {
-    fetchedPosts = documents;
-   return MongoPost.count();
-  })
-  .then(count => {
-  //console.log(documents) ;  // logs all data
-  res.status(200).json({
-    message: 'post fetched sucessfull !',
-    posts: fetchedPosts,
-    maxPosts: count
-  });
-}).catch(error => {
-  res.status(500).json({
-    message: "Fetching Post Failed !"
-  });
-});
-
-  });
+router.get('',PostController.fetchAllData);
 
 
 // to edit an existing post - only user who create can edit it
-  router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
-    console.log(req.file);
-    let imagePath = req.body.imagePath;
-    if(req.file){
-      const url = req.protocol + '://' + req.get("host");
-      imagePath = url + "/images/" + req.file.filename;
-
-    }
-    const post = new MongoPost({
-      _id : req.body.id,
-      title : req.body.title,
-      content : req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.creator
-    });
-    console.log(post);
-    MongoPost.updateOne({_id : req.params.id, creator: req.userData.userId}, post).then( result => {
-      if(result.nModified>0){
-        res.status(200).json({ message: 'Updated Successful !!'});
-      }else{
-        res.status(401).json({ message: 'Not Autorized to Update!!'});
-      }
-
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Couldn't Update the Post !"
-      });
-    });
-
-  });
+router.put("/:id", checkAuth, multer({storage: storage}).single("image"),PostController.updatePost);
 
 
 
   //fetch a Particular record
-  router.get("/:id", (req, res, next) => {
-  MongoPost.findById(req.params.id).then(post => {
-    if(post){
-      res.status(200).json(post);
-    } else{
-      res.status(400).json({message : 'Post no Found !'});
-    }
-  }).catch(error => {
-    res.status(500).json({
-      message: "Post no Found !"
-    });
-  });
-});
+router.get("/:id",PostController.fetchPost);
 
 
 // Delete a particular record
-router.delete("/:id", checkAuth,(req, res, next) => {
-
-    console.log(req.params.id);
-    MongoPost.deleteOne({_id : req.params.id, creator: req.userData.userId}).then( result => {
-      //console.log(result);
-      if(result.n>0){
-        res.status(200).json({ message: " Post Deleted !!"});
-      }else{
-        res.status(401).json({ message: 'Not Autorized to Delete!!'});
-      }
-    }).catch(error => {
-      res.status(500).json({
-        message: "Post Delete Failed !"
-      });
-    });
-
-
-
-  });
+router.delete("/:id", checkAuth,PostController.deletePost);
 
   module.exports = router;
