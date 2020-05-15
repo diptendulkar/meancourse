9bfe05cbea8cb2ee96d703606a010e0117798472
diptendudas <diptendu.tu@gmail.com>
2020-05-15 17:32:42 +0530

    Optimized - backend code

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,47 +1,20 @@
 const express = require("express");
-const multer =  require('multer');
-
-
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file-util');
+
 const router = express.Router();
 
 const PostController = require("../controllers/posts-controller");
 
-const MIME_TYPE_MAP ={
-'image/png': 'png',
-'image/jpeg': 'jpeg',
-'image/jpg': 'jpg',
-};
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let err = new Error("Invalid mime type");
-    if(isValid){
-      err = null;
-    }
-
-    callback(err,"backend/images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const extension = MIME_TYPE_MAP[file.mimetype];
-    callback(null,name + '-' + Date.now()+'.'+extension);
-  }
-});
-
 //add a new post
-router.post("", checkAuth ,multer({storage: storage}).single("image"),PostController.addNewPost);
-
-
+router.post("", checkAuth ,extractFile,PostController.addNewPost);
 
 // fetch all data
 router.get('',PostController.fetchAllData);
 
 
 // to edit an existing post - only user who create can edit it
-router.put("/:id", checkAuth, multer({storage: storage}).single("image"),PostController.updatePost);
-
-
+router.put("/:id", checkAuth, extractFile,PostController.updatePost);
 
   //fetch a Particular record
 router.get("/:id",PostController.fetchPost);
@@ -50,4 +23,4 @@ router.get("/:id",PostController.fetchPost);
 // Delete a particular record
 router.delete("/:id", checkAuth,PostController.deletePost);
 
-  module.exports = router;
+module.exports = router;
